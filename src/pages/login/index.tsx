ce6c088af76c8e582679021b577d070a36f6dc4b
currysoda <currysoda1598@gmail.com>
2025-08-05 14:21:13 +0900

    병합 충돌 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -6,25 +6,6 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-<<<<<<< HEAD
-  return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-4xl font-bold mb-6">로그인</h1>
-      <p className="text-lg mb-4">Walkit에 로그인해보세요!</p>
-      <Link to="/" className="text-blue-600 hover:text-blue-800 underline">
-        홈으로 돌아가기
-      </Link>
-      <br></br>
-      <Link
-        to="/signup"
-        className="text-blue-600 hover:text-blue-800 underline mb-6 block"
-      >
-        회원가입 하러가기
-      </Link>
-      <div style={{ gap: '2rem' }} className="flex flex-col items-center">
-        <GoogleLoginButton />
-        <KakaoLoginButton />
-=======
   const { isAuthenticated, isLoading } = useAuth();
   const navigate = useNavigate();
 
@@ -88,7 +69,6 @@ const Login = () => {
             홈으로 돌아가기
           </Link>
         </div>
->>>>>>> feature/24-bottomsheet
       </div>
     </div>
   );
